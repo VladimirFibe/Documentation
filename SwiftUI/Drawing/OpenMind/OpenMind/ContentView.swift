61854d79f3d2a788e17bb94f15b393673c541ff5
Vladimir <motiw@icloud.com>
2022-10-06 08:12:02 +0600

    add ColorSlider

--- SwiftUI/Drawing/OpenMind/OpenMind/ContentView.swift
+++ SwiftUI/Drawing/OpenMind/OpenMind/ContentView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
-struct ContentView: View {                   
+struct ContentView: View {
+    @EnvironmentObject var cellStore: CellStore
+    @State private var sliderValue = 0.0
     var body: some View {
-        GeometryReader { geometry in
-            BackgroundView(size: geometry.size)
+        VStack {
+            VStack {
+                Text(String(format: "%.2f", sliderValue))
+                ColorSlider(sliderValue: $sliderValue,
+                            range: 0...255,
+                            color: cellStore.selectedCell?.color ?? .blue
+                )
+            }
+            .padding()
+            .frame(height: 80)
+            GeometryReader { geometry in
+                BackgroundView(size: geometry.size)
+            }
         }
     }
 }
@@ -12,5 +25,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(CellStore())
     }
 }
---
+++ SwiftUI/Drawing/OpenMind/OpenMind/Controls/ColorSlider.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+struct ColorSlider: View {
+    @Binding var sliderValue: Double
+    var range: ClosedRange<Double> = 0...1
+    var color: Color = .blue
+    var body: some View {
+        let gradient = LinearGradient(
+            colors: [.black, color, .white],
+            startPoint: .leading,
+            endPoint: .trailing
+        )
+        
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                gradient
+                    .cornerRadius(5)
+                    .frame(height: 10)
+                SliderCircleView(value: $sliderValue,
+                                 range: range,
+                                 sliderWidth: geometry.size.width)
+            }
+            .frame(
+                width: geometry.size.width,
+                height: geometry.size.height,
+                alignment: .center
+            )
+        }
+    }
+}
+
+extension ColorSlider {
+    struct SliderCircleView: View {
+        @Binding var value: Double
+        let range: ClosedRange<Double>
+        let sliderWidth: Double
+        let diameter = 30.0
+        @State private var offset: CGSize = .zero
+        
+        var sliderValue: Double {
+            let percent = offset.width / (sliderWidth - diameter)
+            let value = (range.upperBound - range.lowerBound) * percent + range.lowerBound
+            return value
+        }
+        var body: some View {
+            let drag = DragGesture()
+                .onChanged {
+                    offset.width = clampWidth($0.translation.width)
+                    value = sliderValue
+                }
+            Circle()
+                .foregroundColor(.white)
+                .shadow(color: .gray, radius: 1)
+                .frame(width: diameter, height: diameter)
+                .gesture(drag)
+                .offset(offset)
+        }
+        func clampWidth(_ translation: Double) -> Double {
+            min(sliderWidth - diameter, max(0, offset.width + translation))
+        }
+    }
+}
+struct ColorSlider_Previews: PreviewProvider {
+    @State static var sliderValue: Double = 0
+    static var previews: some View {
+        ColorSlider(sliderValue: $sliderValue)
+            .padding(50)
+            .background(.secondary)
+    }
+}
